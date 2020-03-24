b4cc166c1d9bba80dc7c76754f88d241a2f57ed3
Alexander Rowland <rowlanda@protonmail.com>
2020-03-23 20:53:13 -0400

    Add scripts directory with mouse input script

---
+++ scripts/read_mouse.py
@@ -0,0 +1,7 @@
+import struct
+
+f = open("/dev/input/mice", "rb")
+# Open the file in read-binary mode
+while 1:
+    data = f.read(3)    # Reads the 3 bytes
+    print(struct.unpack('3b', data))    # Unpacks the bytes to integers
